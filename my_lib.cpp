d3a8c80077161e5c0495cbc6b9959be5cf5ab685
SergiyOliynik <45273054+SergiyOliynik@users.noreply.github.com>
2019-12-20 18:45:48 +0200

    Create my_lib.cpp

---
+++ my_lib.cpp
@@ -0,0 +1,10 @@
+#include "my_lib.h" // header in local directory
+#include <iostream> // header in standard library
+
+using namespace N;
+using namespace std;
+
+void my_lib::do_something()
+{
+    cout << "Doing something!" << endl;
+}
